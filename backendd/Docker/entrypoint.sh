62c2feb1aebb69e4f92714b15195f3ea56bf9808
Timo255 <104308259+Timo255@users.noreply.github.com>
2025-08-23 13:34:35 +0300

    backend commit

--- backendd/Docker/entrypoint.sh
+++ backendd/Docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -f "vendor/autoload.php" ]; then
-    composer install --no-dev --optimize-autoloade
+    composer install --no-progress --no-interaction
 fi
 
 if [ ! -f ".env" ]; then
@@ -11,12 +11,12 @@ else
     echo "env file exists."
 fi
 
-# php artisan migrate
+php artisan migrate --force
 # php artisan db:seed
 # php artisan breeze:install api
 # php artisan key:generate
-# php artisan cache:clear
-# php artisan config:clear
-# php artisan route:clear
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
 php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
 exec docker-php-entrypoint "$@"
