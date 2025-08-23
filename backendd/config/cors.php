227511d2024c042f8236b6747052708e62a64c25
Timo255 <104308259+Timo255@users.noreply.github.com>
2025-08-23 16:31:43 +0300

    Update cors.php

--- backendd/config/cors.php
+++ backendd/config/cors.php
@@ -1,40 +1,56 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
     'paths' => [
-    'api/*',
-    'sanctum/csrf-cookie',
-    'login',
-    'logout',
-    'register'	
+        'api/*',
+        'sanctum/csrf-cookie',
+        'login',
+        'logout',
     ],
-
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['https://laravel-e-commerce-frontend.onrender.com','https://laravel-e-commerce-vd78.onrender.com'],
-
-    'allowed_origins_patterns' => [],
-
+    'allowed_origins' => [
+        'https://laravel-e-commerce-frontend.onrender.com',
+    ],
     'allowed_headers' => ['*'],
-
-    'exposed_headers' => [],
-
-    'max_age' => 0,
-
     'supports_credentials' => true,
-
 ];
+
+
+// return [
+
+//     /*
+//     |--------------------------------------------------------------------------
+//     | Cross-Origin Resource Sharing (CORS) Configuration
+//     |--------------------------------------------------------------------------
+//     |
+//     | Here you may configure your settings for cross-origin resource sharing
+//     | or "CORS". This determines what cross-origin operations may execute
+//     | in web browsers. You are free to adjust these settings as needed.
+//     |
+//     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+//     |
+//     */
+
+//     'paths' => [
+//     'api/*',
+//     'sanctum/csrf-cookie',
+//     'login',
+//     'logout',
+//     'register'	
+//     ],
+
+//     'allowed_methods' => ['*'],
+
+//     'allowed_origins' => ['https://laravel-e-commerce-frontend.onrender.com','https://laravel-e-commerce-vd78.onrender.com'],
+
+//     'allowed_origins_patterns' => [],
+
+//     'allowed_headers' => ['*'],
+
+//     'exposed_headers' => [],
+
+//     'max_age' => 0,
+
+//     'supports_credentials' => true,
+
+// ];
