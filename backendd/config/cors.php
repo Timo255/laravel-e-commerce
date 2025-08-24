f36535f14a9eca8f456f407781cc36d7b5f8ec9e
Timo255 <104308259+Timo255@users.noreply.github.com>
2025-08-24 17:06:44 +0300

    Update cors.php

--- backendd/config/cors.php
+++ backendd/config/cors.php
@@ -2,24 +2,16 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout','register','forgot-password','reset-password'],
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+        'login',
+        'logout'
+    ],
 
     'allowed_methods' => ['*'],
 
-     'allowed_origins' => [
+    'allowed_origins' => [
         'https://laravel-e-commerce-frontend.onrender.com',
         'http://localhost:5173',
     ],
