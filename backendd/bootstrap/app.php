27e0d3c1a1b1e935cfffaf304730779d5d4ceca9
Timo255 <104308259+Timo255@users.noreply.github.com>
2025-08-24 12:06:19 +0300

    Update app.php

--- backendd/bootstrap/app.php
+++ backendd/bootstrap/app.php
@@ -20,7 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
-        $middleware->validateCsrfTokens(except: ['api/*']);
+        $middleware->validateCsrfTokens();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
