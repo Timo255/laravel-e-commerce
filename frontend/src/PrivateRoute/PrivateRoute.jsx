7b6d570eae21f5bb0e35877d8dce9597fb18400b
Timothy <104308259+Timo255@users.noreply.github.com>
2025-09-21 19:13:12 +0300

    Update PrivateRoute.jsx

--- frontend/src/PrivateRoute/PrivateRoute.jsx
+++ frontend/src/PrivateRoute/PrivateRoute.jsx
@@ -1,66 +1,23 @@
-import React, { useEffect } from "react";
+import React from "react";
 import useAuth from "../hooks/useAuth";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 
-const PrivateRoute = ({ children }) => {
-  const { auth, isLoading, setAuth, getUser,setLoading } = useAuth();
+const PrivateRoute = () => {
+  const { auth, isLoading } = useAuth();
   const location = useLocation();
 
-  console.log(auth);
-  // useEffect(() => {
-  //   const userGet = async () => {
-  //     try {
-  //       if (!auth?.name) {
-  //         setLoading(true)
-  //         await getUser();
-  //         setLoading(false)
-  //       }
-  //     } catch (error) {
-  //       const res = error.response;
-  //       if (res && res.status === 401) {
-  //         // console.log(res.data.errors);
-  //         // setErrors(res.data.errors);
-  //         setLoading(false);
-  //       }
-  //     }
-  //   };
-  //   userGet();
-  // },[]);
-  //  if(auth?.name){
-  //       return children;
-  //   }
+  if (isLoading) {
+    // Still checking token → don’t redirect yet
+    return <div>Loading...</div>;
+  }
 
-  return (
-    <>
-      {/* {
-        auth?.name && <Outlet />
-      } */}
-      {/* {
-        auth?.name ? <Outlet /> : <Navigate to={"/login"} state={{ from: location }} replace />
-      } */}
+  // Only allow if user has a name (or any other identifier you store)
+  if (auth && auth.name) {
+    return <Outlet />;
+  }
 
-      {/* {isLoading ? (
-        <div>
-          <div className="loading">
-            <img src="/Rolling@1x-1.0s-200px-200px.svg" alt="" />
-          </div>
-        </div>
-      ) : auth ? (
-        <Outlet /> ) : <Navigate to={"/login"} state={{ from: location }} replace />
-       } */}
-
-     {auth? (
-        <Outlet />
-      ) : (
-        <Navigate to={"/login"} state={{ from: location }} replace />
-      )}
-    
-
-      {/* : (
-        <Navigate to={"/login"} state={{ from: location }} replace />
-      ) */}
-    </>
-  );
+  // Otherwise, go to login
+  return <Navigate to="/login" state={{ from: location }} replace />;
 };
 
 export default PrivateRoute;
